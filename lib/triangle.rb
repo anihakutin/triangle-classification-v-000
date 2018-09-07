fb9a06437c3eb52e341b3845d4b3f98489b072bb
Heshie Brody <4hbrody@gmail.com>
2018-09-07 03:27:23 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end
   def kind
     sides = [side1, side2, side3]
-    if sides.any?{|s| s < 1}
+    if sides.any?{|s| s <= 0}
       raise TriangleError
     elsif [side1, side2, side3].uniq.size == 1
       :equilateral
