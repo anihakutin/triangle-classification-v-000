74b2536bd9d37709954a8c528eccfe1e91d56172
Heshie Brody <4hbrody@gmail.com>
2018-09-07 03:04:29 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,10 +10,10 @@ class Triangle
     sides = [side1, side2, side3]
     if sides.all? {|side| sides[0] == side}
       :equilateral
-    # elsif sides.any? {|side| sides[0] == side} && !not_eql?(sides)
-    #   :isosceles
     elsif not_eql?(sides)
       :scalene
+    elsif sides.any? {|side| sides[0] == side} && !not_eql?(sides)
+      :isosceles
     else
       raise TriangleError
     end
