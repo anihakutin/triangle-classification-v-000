9c46bfadf832a3ac97f52f10669708efd8b9b4ac
Heshie Brody <4hbrody@gmail.com>
2018-09-07 02:58:35 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
     sides = [side1, side2, side3]
     if sides.all? {|side| sides[0] == side}
       :equilateral
-    elsif sides.any? {|side| sides[0] == side} && !sides.all? {|side| sides[0] != side} 
+    elsif sides.any? {|side| sides[0] == side && sides[1] != side} 
       :isosceles
     elsif sides.all? {|side| sides[0] != side}
       :scalene
